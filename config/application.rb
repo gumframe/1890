3fd8f65f2cab2e42a86a62ecf8ec3dd7094ef819
Kendrick Smith <tvbuzzbee@gmail.com>
2023-04-13 19:07:37 -0700

    updated application.rb to load inner folder for images

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module HoldingCompany
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.paths << Rails.root.join('app', 'assets', 'images')
+
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+        config.assets.paths << path
+    end
   end
 end
